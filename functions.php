aca3f2614bd601685489c9e17b08052712eeec23
Michael Bryne <mbryne@une.edu.au>
2014-05-09 12:05:54 +1000

    Tweaks

--- functions.php
+++ functions.php
@@ -5,42 +5,44 @@
  * @package Apper Base Theme
  */
 
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
-}
+#
+#   ACTIONS
+#
 
-if ( ! function_exists( 'apper_setup' ) ) :
+add_action( 'after_setup_theme', 'apper_theme_setup' );
+
+#
+#   FUNCTIONS
+#
+
+if ( ! isset( $content_width ) )
+	$content_width = 640; /* pixels */
+
 /**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
+ * Enqueue scripts and styles.
  */
-function apper_setup() {
+function apper_scripts() {
+
+    wp_enqueue_style( 'apper-style', get_stylesheet_uri() );
+    wp_enqueue_script( 'jquery-placeholder', get_template_directory_uri() . '/js/jquery.placeholder.js', array(), '20120206', true );
+    wp_enqueue_script( 'apper-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+    wp_enqueue_script( 'apper-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+
+}
+add_action( 'wp_enqueue_scripts', 'apper_scripts' );
+
+function apper_theme_setup() {
 
 	add_filter('show_admin_bar', 'remove_admin_bar'); // Remove Admin bar
-
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Apper Base Theme, use a find and replace
-	 * to change 'apper' to the name of your theme in all the template files
-	 */
 	load_theme_textdomain( 'apper', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -51,27 +53,6 @@ function apper_setup() {
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array( 'comment-list', 'search-form', 'comment-form', ) );
 }
-endif; // apper_setup
-add_action( 'after_setup_theme', 'apper_setup' );
-
-
-/**
- * Enqueue scripts and styles.
- */
-function apper_scripts() {
-	wp_enqueue_style( 'apper-style', get_stylesheet_uri() );
-
-    wp_enqueue_script( 'jquery-placeholder', get_template_directory_uri() . '/js/jquery.placeholder.js', array(), '20120206', true );
-
-    wp_enqueue_script( 'apper-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'apper-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'apper_scripts' );
 
 
 
